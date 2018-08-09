ea50c0b4c93d69a2f1bae4460da76d7ca7e7e060
GiovanniGatDev <giovannigatfut@gmail.com>
2018-08-09 17:56:11 -0500

    Agregando los datos de empleado

--- src/routes/empleados.js
+++ src/routes/empleados.js
@@ -14,9 +14,27 @@ router.get('/:id', (req, res) => {
     const { id } = req.params;
     mysqlConection.query('SELECT * FROM empleados WHERE id = ?', [id], (err, rows, fields) => {
         if (!err) {
-            res.json(rows);
+            res.json(rows[0]);
         } else {
             console.log(err);
+        }
+    });
+});
+
+//Crear un dato 
+router.post('/', (req, res) => {
+    const { id, name, salario } = req.body;
+    const query = `
+    SET @id = ?;
+    SET @name = ?;
+    SET @salario = ?;
+    CALL empleadosaddoredit(@id, @name, @salario);
+    `;
+    mysqlConection.query(query, [id, name, salario], (err, rows, fields) => {
+        if (!err) {
+            res.json({ Status: 'Empleado guardado' });
+        } else {
+            console.log(err)
         }
     });
 });
