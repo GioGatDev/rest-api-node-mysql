0af7d13dd5916e71dff3557553e09a2ad955c7ba
GiovanniGatDev <giovannigatfut@gmail.com>
2018-08-09 19:34:03 -0500

    Agregando CRUD Rest API

--- src/routes/empleados.js
+++ src/routes/empleados.js
@@ -25,16 +25,36 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
     const { id, name, salario } = req.body;
     const query = `
-    SET @id = ?;
-    SET @name = ?;
-    SET @salario = ?;
-    CALL empleadosaddoredit(@id, @name, @salario);
+    CALL empreadosaddoredit(?, ?, ?);
     `;
     mysqlConection.query(query, [id, name, salario], (err, rows, fields) => {
         if (!err) {
             res.json({ Status: 'Empleado guardado' });
         } else {
-            console.log(err)
+            console.log(err);
+        }
+    });
+});
+router.put('/:id', (req, res) => {
+    const { name, salario } = req.body;
+    const { id } = req.params;
+    const query = 'CALL empreadosaddoredit(?, ?, ?)';
+    mysqlConection.query(query, [id, name, salario], (err, rows, fields) => {
+        if (!err) {
+            res.json({ Status: 'Empleado editado' });
+        } else {
+            console.log(err);
+        }
+    });
+
+});
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    mysqlConection.query('DELETE FROM empleados WHERE id = ?', [id], (err, rows, fields) => {
+        if (!err) {
+            res.json({ Status: 'Empleado Eliminado' });
+        } else {
+            console.log(err);
         }
     });
 });
