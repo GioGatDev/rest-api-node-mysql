2143fa32c29678a0bc5c403235a9083d73807855
GatDev <giovannigatfut@gmail.com>
2025-02-07 11:59:04 -0600

    correccion de procedimiento almacenado
    asegurando permisos del procedimiento almacenado

--- db/procedimiento.sql
+++ db/procedimiento.sql
@@ -1,20 +1,25 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `empreadosaddoredit`(
-	IN _id INT,
+DELIMITER //
+
+CREATE PROCEDURE `empreadosaddoredit`(
+    IN _id INT,
     IN _name VARCHAR(45),
     IN _salario INT
 )
 BEGIN
-	IF _id = 0 THEN
-    INSERT INTO empleados (name, salario)
-		VALUES (_name, _salario);
+    IF _id = 0 THEN
+        INSERT INTO empleados (name, salario)
+        VALUES (_name, _salario);
         
-		SET _id = LAST_INSERT_ID();
-	ELSE
-		UPDATE empleados
+        SET _id = LAST_INSERT_ID();
+    ELSE
+        UPDATE empleados
         SET 
-		name = _name,
+        name = _name,
         salario = _salario
         WHERE id = _id;
-	END IF;
+    END IF;
+    
     SELECT _id AS id;
-END
+END //
+
+DELIMITER ;
